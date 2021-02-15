7c370a2cb8e3730a24c825d3ec26a27c412f7af0
Jason Messler <jason.messler.1@gmail.com>
2021-02-15 13:16:34 -0500

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_11_version_2
+{
+    class Movie
+    {
+
+        private string _category;
+        private string _title;
+
+        public Movie(string title, string category)
+        {
+            _title = title;
+            _category = category;
+        }
+
+        public Movie()
+        {
+
+        }
+
+        public void GetSciFi()
+        {
+            var movieList = PrintList();
+            var scifiTitles = new List<string>();
+
+            foreach (var movie in movieList)
+            {
+                if (movie._category == "scifi")
+                {
+                    scifiTitles.Add(movie._title);
+
+
+                }
+            }
+            scifiTitles.Sort();
+            foreach (var film in scifiTitles)
+            {
+                Console.WriteLine(film);
+            }
+        }
+
+        public void GetDrama()
+        {
+            var movieList = PrintList();
+            var dramaTitles = new List<string>();
+
+
+            foreach (var movie in movieList)
+            {
+                if (movie._category == "drama")
+                {
+                    dramaTitles.Add(movie._title);
+                }
+            }
+            dramaTitles.Sort();
+            foreach (var film in dramaTitles)
+            {
+                Console.WriteLine(film);
+            }
+        }
+
+        public void GetAnimated()
+        {
+            var movieList = PrintList();
+            var animatedTitles = new List<string>();
+
+            foreach (var movie in movieList)
+            {
+                if (movie._category == "animated")
+                {
+                    animatedTitles.Add(movie._title);
+                }
+            }
+            animatedTitles.Sort();
+            foreach (var film in animatedTitles)
+            {
+                Console.WriteLine(film);
+
+            }
+        }
+
+        public void GetHorror()
+        {
+            var movieList = PrintList();
+            var horrorTitles = new List<string>();
+
+            foreach (var movie in movieList)
+            {
+                if (movie._category == "horror")
+                {
+                    horrorTitles.Add(movie._title);
+                }
+            }
+            horrorTitles.Sort();
+            foreach (var film in horrorTitles)
+            {
+                Console.WriteLine(film);
+            }
+        }
+
+        private List<Movie> PrintList()
+        {
+            var movieList = new List<Movie>();
+
+
+            movieList.Add(new Movie("Alien", "scifi"));
+            movieList.Add(new Movie("The Terminator", "scifi"));
+            movieList.Add(new Movie("Blade Runner", "scifi"));
+            movieList.Add(new Movie("Predator", "scifi"));
+            movieList.Add(new Movie("Goodfellas", "drama"));
+            movieList.Add(new Movie("Amadeus", "drama"));
+            movieList.Add(new Movie("To kill a mockingbird", "drama"));
+            movieList.Add(new Movie("The grapes of wrath", "drama"));
+            movieList.Add(new Movie("Akira", "animated"));
+            movieList.Add(new Movie("Fritz the cat", "animated"));
+            movieList.Add(new Movie("The secret of NIMH", "animated"));
+            movieList.Add(new Movie("Sausage Party", "animated"));
+            movieList.Add(new Movie("The Exorcist", "horror"));
+            movieList.Add(new Movie("The Thing", "horror"));
+            movieList.Add(new Movie("The Void", "horror"));
+            movieList.Add(new Movie("Reanimator", "horror"));
+
+            return movieList;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Lab_11_version_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Movie List Application!");  // intro line- only used once
+            
+            string userContinue;
+            do
+            {
+                bool isInvalid;
+                do
+                {
+                    var result = GetNumberSafely(); // continue method
+                    MovieChoice(result);
+                    Console.WriteLine("Would you like to continue? (y/n)");
+                    userContinue = Console.ReadLine().ToLower();
+                    isInvalid = userContinue != "y" && userContinue != "n";
+                    if (isInvalid)
+                    {
+                        Console.WriteLine("Please enter a 'y' or 'n'!" + "\n");
+                    }
+                } while (isInvalid);
+            } while (userContinue == "y");
+
+
+
+            Console.WriteLine("Goodbye!"); // goodbye
+            Console.ReadLine();
+        }
+        private static int GetNumberSafely()
+        {
+            int result;
+            string userMovie;
+            bool isValid;
+
+            
+            do
+            {
+                Console.WriteLine("What category are you interested in?"); // user input section
+                Console.WriteLine("For Scifi, enter 1");
+                Console.WriteLine("For Animated, enter 2");
+                Console.WriteLine("For Horror, enter 3");
+                Console.Write("For Drama, enter 4" + "\n\n" + "Enter your choice here: ");
+
+                userMovie = Console.ReadLine();
+                isValid = int.TryParse(userMovie, out result);
+            } while (!isValid);
+
+            return result;
+        }
+
+        private static bool MovieChoice(int response) // calling methods to main
+        {
+            if (response == 1)
+            {
+                var Scifi = new Movie();
+                Scifi.GetSciFi();
+            }
+            else if (response == 2)
+            {
+                var Animated = new Movie();
+                Animated.GetAnimated();
+            }
+            else if (response == 3)
+            {
+                var Horror = new Movie();
+                Horror.GetHorror();
+            }
+            else if (response == 4)
+            {
+                var Drama = new Movie();
+                Drama.GetDrama();
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number!"); // safety number validity
+            }
+            return false;
+        }
+    }
+}
